f154fcbde7d40dc8546ca8821eb8b0f8a8fe12f1
a-garey <agarey@uchicago.edu>
2020-08-05 15:25:07 -0400

    Add soup kitches
    Html page for soup kitchens

---
+++ food_finder_project_plus/main_app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('volunteer', views.volunteer),
+    path('login', views.login),
+    path('process_login', views.process_login),
+    path('registration', views.registration),
+    path('register', views.register),
+    path('profile', views.profile),
+    path('change_picture', views.change_picture),
+    path('edit_profile', views.edit_profile),
+    path('update_profile', views.update_profile),
+    path('delete', views.delete),
+    path('logout', views.logout),
+    path('search', views.search),
+    path('zipsearch', views.zipsearch),
+    path('backup', views.backup),
+]
